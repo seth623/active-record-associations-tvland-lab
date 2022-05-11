3208f3184540c534a9281c1c3dce0a70536238bb
seth <seth623@gmail.com>
2022-05-10 20:38:16 -0600

    Completed Assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
 class Actor < ActiveRecord::Base
+    has_many :characters 
+    has_many :shows, through: :characters 
+
+    def full_name 
+        "#{self.first_name}" + " #{self.last_name}"
+    end 
+    
+    def list_roles
+        
+        roles = []
+
+        self.characters.map do |c|
+            role = "#{c.name} - #{c.show.name}"
+            roles << role 
+        end
+
+        roles 
+
+    end 
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,20 @@
 class Show < ActiveRecord::Base
+    has_many :actors 
+    has_many :characters 
+    has_many :actors, through: :characters 
+    belongs_to :network 
+
+    def actors_list
+        
+        actors = []
+
+        self.actors.map do |a|
+            actor_name = a.full_name 
+            actors << actor_name 
+        end 
+
+        actors 
+
+    end 
   
 end
